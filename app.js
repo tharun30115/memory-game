6c3251213cf62e542bee01416391761ce75345bd
Tharun <tharungowda30115@gmail.com>
2025-01-30 22:30:43 +0530

    app js

--- app.js
+++ app.js
@@ -25,4 +25,29 @@ const items = [
     { name: "toucan", image: "assets/toucan.png" },
   ];
   
-  
+  //Initial Time
+let seconds = 0,
+minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+winCount = 0;
+
+//For timer
+const timeGenerator = () => {
+seconds += 1;
+//minutes logic
+if (seconds >= 60) {
+  minutes += 1;
+  seconds = 0;
+}
+//format time before displaying
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+//For calculating moves
+const movesCounter = () => {
+movesCount += 1;
+moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
