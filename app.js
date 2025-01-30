5913e7bbfcbbcd9e22a8a0b5cc878f4f60cc021d
Tharun <tharungowda30115@gmail.com>
2025-01-30 22:30:11 +0530

    app js

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ let cards;
 let interval;
 let firstCard = false;
 let secondCard = false;
+
+const items = [
+    { name: "bee", image: "assets/bee.png" },
+    { name: "crocodile", image: "assets/crocodile.png" },
+    { name: "macaw", image: "assets/macaw.png" },
+    { name: "gorilla", image: "assets/gorilla.png" },
+    { name: "tiger", image: "assets/tiger.png" },
+    { name: "monkey", image: "assets/monkey.png" },
+    { name: "chameleon", image: "assets/chameleon.png" },
+    { name: "piranha", image: "assets/piranha.png" },
+    { name: "anaconda", image: "assets/anaconda.png" },
+    { name: "sloth", image: "assets/sloth.png" },
+    { name: "cockatoo", image: "assets/cockatoo.png" },
+    { name: "toucan", image: "assets/toucan.png" },
+  ];
+  
+  
